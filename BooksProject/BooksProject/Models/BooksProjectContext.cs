5152f82eeb89e9ea3f092033ae062503ea29f71b
renetag97 <nachossrg@gmail.com>
2018-11-21 21:26:10 +0200

    BooksProject

---
+++ BooksProject/BooksProject/Models/BooksProjectContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BooksProject.Models
+{
+    public class BooksProjectContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public BooksProjectContext() : base("name=BooksProjectContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<BooksProject.Models.Book> Books { get; set; }
+
+        public System.Data.Entity.DbSet<BooksProject.Models.Genre> Genres { get; set; }
+
+        public System.Data.Entity.DbSet<BooksProject.Models.Writer> Writers { get; set; }
+    }
+}
---
+++ BooksProject/BooksProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BooksProject.Startup))]
+namespace BooksProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
